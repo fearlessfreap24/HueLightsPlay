5202e8b52327b9d3bad9610ce67d19e40cabd260
fearlessfreap24 <dylanaperez@yahoo.com>
2019-12-01 07:18:43 +0000

    made flashing portion

--- lights.py
+++ lights.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import time
 from dotenv import load_dotenv
 
 # read .env file to get Hue Key
@@ -19,5 +20,24 @@ dict = r.json()
 
 # print name and light state for each light.
 for i in dict:
-    print("{}, on: {}".format(dict[i]['name'], dict[i]['state']['on']))
+    print("{}, {}, on: {}".format(i, dict[i]['name'], dict[i]['state']['on']))
+
+# blinking light
+
+# get my light info
+dyllight = dict['5']
+
+# get state into variable and create opposite state
+onoff = dyllight['state']['on']
+secondstate = not onoff
+
+# make it flash 5 times with loop
+# the json= part took a second to figure out
+cnt = 0
+while cnt != 5:
+	requests.put(url+"/lights/5/state", json={'on': secondstate})
+	time.sleep(2)
+	requests.put(url+"/lights/5/state", json={'on': onoff})
+	time.sleep(2)
+	cnt += 1
 
