8d374f284dbf0e8983e5a2f9b4275f8c7eb9cf8a
fearlessfreap24 <dylanaperez@yahoo.com>
2020-01-11 08:18:50 +0000

    added link to lights page to turn off a specific light

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,7 @@ import requests
 import os
 import time
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask,redirect
 
 # read .env file to get Hue Key
 load_dotenv()
@@ -26,7 +26,7 @@ def getLights():
     light = "<table><tr><th>Number</th><th>Name</th><th>State</th></tr>"
     for i in dict:
         light = light + "<tr><td>{}</td><td>{}</td><td>{}</td></tr>".format(i, dict[i]['name'], dict[i]['state']['on'])
-    light = light + "</table>"
+    light = light + "</table><p><a href=\"./dylan\">Turn on/off my light</a></p>"
     return light
 
 @app.route('/')
@@ -41,3 +41,11 @@ def hello():
 def lights():
     return getLights()
 
+@app.route('/dylan')
+def dylan():
+	light5 = requests.get("http://" + hip + "/api/" + hk + "/lights/5/")
+	control5 = light5.json()
+	currStatus = control5['state']['on']
+	url = "http://" + hip + "/api/" + hk
+	requests.put(url+"/lights/5/state", json={'on': not currStatus})
+	return redirect("./lights")
