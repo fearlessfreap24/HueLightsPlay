efcfe7e6fd4223aaf2e505b0d09bb2e0197cf5aa
dylanptcu <dylan.perez@tcu.edu>
2020-04-08 15:27:19 -0500

    changed label for onoff and intensity

--- roomform.py
+++ roomform.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired
 
 class RoomForm(FlaskForm):
     room = SelectField(u'Room', coerce=int, validators=[DataRequired()])
-    onoff = BooleanField('On/Off')
-    intensity = StringField('Intensity\n1-256', validators=[DataRequired()])
+    onoff = BooleanField('Turn on light?')
+    intensity = StringField('Intensity\n1-254', validators=[DataRequired()])
     submit = SubmitField('Submit')
