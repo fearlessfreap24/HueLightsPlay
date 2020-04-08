48aea4403e5e5fdc0115c2ce813006ce00bfa833
dylanptcu <dylan.perez@tcu.edu>
2020-04-07 19:29:29 -0500

    changed room to select field

--- roomform.py
+++ roomform.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, SelectField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class RoomForm(FlaskForm):
-    room = StringField('Room', validators=[DataRequired()])
+    room = SelectField(u'Room', validators=[DataRequired()])
     onoff = BooleanField('On/Off')
     intensity = StringField('Intensity\n1-256')
     submit = SubmitField('Submit')
