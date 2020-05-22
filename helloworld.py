14ff65d09ef4e9b6224d15812bd32fd085e48acb
fearlessfreap24 <dylanaperez@yahoo.com>
2020-05-21 21:46:48 -0500

    reformatted helloworld.py and removed lights.py and goodnight.py

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask,redirect,render_template, flash
+from flask import Flask, redirect, render_template, flash
 from config import Config
 from roomform import RoomForm
 
@@ -13,47 +13,47 @@ hip = app.config["HUEIP"]
 
 # routine to create base url
 def url():
-	return str("http://" + hip + "/api/" + hk)
+    return str("http://" + hip + "/api/" + hk)
 
 
 # a dict to pass info for nav bar to jinja
 def headerinfo():
-	return {'Home': "/", 'Lights': "/lights", 'Special Functions': "/special", 'Rooms': "/rooms"}
+    return {'Home': "/", 'Lights': "/lights", 'Special Functions': "/special", 'Rooms': "/rooms"}
 
 
 # method to retrieve room names
 def getrooms():
-	# http request for rooms
-	r = requests.get(url()+"/groups")
-	# turn lights JSON into a dict for reading
-	dicto = r.json()
-	return dicto
+    # http request for rooms
+    r = requests.get(url() + "/groups")
+    # turn lights JSON into a dict for reading
+    dicto = r.json()
+    return dicto
 
 
 def getlights():
-	# getting lights data
-	r = requests.get(url() + "/lights")
-	# turn lights JSON into a dict for reading
-	dicto = r.json()
-	return dicto
+    # getting lights data
+    r = requests.get(url() + "/lights")
+    # turn lights JSON into a dict for reading
+    dicto = r.json()
+    return dicto
 
 
 # home page
 @app.route('/')
 def index():
-	return render_template("index.html", header=headerinfo())
+    return render_template("index.html", header=headerinfo())
 
 
 # test page
 @app.route('/hello/')
 def hello():
-	return 'Hello World'
+    return 'Hello World'
 
 
 # display lights and light status
 @app.route('/lights')
 def lights():
-	# get lights information
+    # get lights information
     dicto = getlights()
 
     # render the template and pass dict to template
@@ -64,37 +64,35 @@ def lights():
 @app.route('/dylan')
 def dylan():
     # request info for light 5
-	light5 = requests.get(url()+"/lights/5/")
+    light5 = requests.get(url() + "/lights/5/")
 
-	# create dict for reading
-	control5 = light5.json()
+    # create dict for reading
+    control5 = light5.json()
 
-	# remember the current state of the light
-	currStatus = control5['state']['on']
+    # remember the current state of the light
+    currStatus = control5['state']['on']
 
-	# change light state to the opposite of the current state
-	requests.put(url()+"/lights/5/state", json={'on': not currStatus})
+    # change light state to the opposite of the current state
+    requests.put(url() + "/lights/5/state", json={'on': not currStatus})
 
-	# return to the lights.html page
-	return redirect("./lights")
+    # return to the lights.html page
+    return redirect("./lights")
 
 
 # function to turn off leopard lamp and dylan light
 @app.route('/leopard')
 def leopard():
+    # change light state to off
+    requests.put(url() + "/lights/5/state", json={'on': False})
+    requests.put(url() + "/lights/3/state", json={'on': False})
 
-	# change light state to off
-	requests.put(url()+"/lights/5/state", json={'on': False })
-	requests.put(url()+"/lights/3/state", json={'on': False })
-
-	# return to the lights.html page
-	return redirect("./lights")
+    # return to the lights.html page
+    return redirect("./lights")
 
 
 # a page for links to leopard() and dylan()
 @app.route('/special')
 def special():
-
     # render special.html and pass header info
     return render_template("./special.html", header=headerinfo())
 
@@ -102,7 +100,7 @@ def special():
 # a page to display rooms and rooms stauts
 @app.route('/rooms')
 def rooms():
-	# get room information
+    # get room information
     groups = getrooms()
 
     # render rooms.html and pass header and room info to page
@@ -112,61 +110,59 @@ def rooms():
 # morning on routine
 @app.route('/morning')
 def morning():
+    # turn on bedroom lights at full brightness
+    requests.put(url() + "/groups/2/action", json={'on': True, 'bri': 254})
 
-	# turn on bedroom lights at full brightness
-	requests.put(url() + "/groups/2/action", json={'on': True, 'bri': 254})
+    # turn on living room light at full brightness
+    requests.put(url() + "/groups/1/action", json={'on': True, 'bri': 254})
 
-	# turn on living room light at full brightness
-	requests.put(url() + "/groups/1/action", json={'on': True, 'bri': 254})
+    # turn on hall lights at half brightness
+    requests.put(url() + "/groups/5/action", json={'on': True, 'bri': 127})
 
-	# turn on hall lights at half brightness
-	requests.put(url() + "/groups/5/action", json={'on': True, 'bri': 127})
-
-	# send back to lights page
-	return redirect("./lights")
+    # send back to lights page
+    return redirect("./lights")
 
 
 # routine for darken ship
 @app.route('/dogzebra')
 def dogzebra():
+    # living room = 1
+    # master bedroom = 2
+    # paige's office = 3
+    # dylan's office = 4
+    # hallway = 5
+    requests.put(url() + "/groups/1/action", json={'on': False})
+    requests.put(url() + "/groups/2/action", json={'on': False})
+    requests.put(url() + "/groups/3/action", json={'on': False})
+    requests.put(url() + "/groups/4/action", json={'on': False})
+    requests.put(url() + "/groups/5/action", json={'on': False})
 
-	# living room = 1
-	# master bedroom = 2
-	# paige's office = 3
-	# dylan's office = 4
-	# hallway = 5
-	requests.put(url() + "/groups/1/action", json={'on': False})
-	requests.put(url() + "/groups/2/action", json={'on': False})
-	requests.put(url() + "/groups/3/action", json={'on': False})
-	requests.put(url() + "/groups/4/action", json={'on': False})
-	requests.put(url() + "/groups/5/action", json={'on': False})
-
-	# send back to lights page
-	return redirect("./lights")
+    # send back to lights page
+    return redirect("./lights")
 
 
 @app.route('/roomform', methods=['GET', 'POST'])
 def roomform():
-	# get dict of rooms
-	groups = getrooms()
-	# instantiate form
-	form = RoomForm()
-	# add choices to SelectField using rooms
-	form.room.choices = [(int(room), groups[room]['name']) for room in groups]
-	# check to see if form validated
-	if form.validate_on_submit():
-		# extract data from submitted form
-		room = form.room.data
-		intensity = form.intensity.data
-		onoff = form.onoff.data
+    # get dict of rooms
+    groups = getrooms()
+    # instantiate form
+    form = RoomForm()
+    # add choices to SelectField using rooms
+    form.room.choices = [(int(room), groups[room]['name']) for room in groups]
+    # check to see if form validated
+    if form.validate_on_submit():
+        # extract data from submitted form
+        room = form.room.data
+        intensity = form.intensity.data
+        onoff = form.onoff.data
 
-		# send data to Hue to change room setting per form
-		requests.put(url() + f"/groups/{room}/action", json={'on': onoff, 'bri': int(intensity)})
-		# redirect to rooms.html to see changes
-		return redirect("./rooms")
-	# if validation failed or new form
-	else:
-		# get errors from submitted form
-		flash(form.errors)
-		# display roomform.html
-		return render_template('./roomform.html', header=headerinfo(), form=form)
+        # send data to Hue to change room setting per form
+        requests.put(url() + f"/groups/{room}/action", json={'on': onoff, 'bri': int(intensity)})
+        # redirect to rooms.html to see changes
+        return redirect("./rooms")
+    # if validation failed or new form
+    else:
+        # get errors from submitted form
+        flash(form.errors)
+        # display roomform.html
+        return render_template('./roomform.html', header=headerinfo(), form=form)
