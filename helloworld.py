a416c8067f825d72e1376c3ec9fd78b24e5cd3ce
Dylan Perez <24607891+fearlessfreap24@users.noreply.github.com>
2022-10-14 14:58:34 -0500

    changes to production config

--- helloworld.py
+++ helloworld.py
@@ -389,7 +389,7 @@ def bush_rotation():
 
 if __name__ == "__main__":
     # Testing
-    app.run(debug=True, port=5001)
+    # app.run(debug=True, port=5001)
     # Production
-    # app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
     # print(ssStatus())
