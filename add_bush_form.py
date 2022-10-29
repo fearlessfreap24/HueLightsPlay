b7a6ee3ff6f0c480915defaf80059deb48e1da48
Dylan Perez <24607891+fearlessfreap24@users.noreply.github.com>
2022-10-29 07:21:55 -0500

    changed bush_type to select field

--- add_bush_form.py
+++ add_bush_form.py
@@ -1,12 +1,18 @@
-from wtforms import StringField, DateField, IntegerField, SubmitField
+from wtforms import StringField, DateField, IntegerField, SelectField
 from wtforms.validators import NumberRange, DataRequired, Length
 from flask_wtf import FlaskForm
 
 class Add_Bush(FlaskForm):
-    bush_type = StringField("Bush Type", validators=[DataRequired(), Length(min=3, max=25)])
+    bush_type = SelectField(
+        "Bush Type",
+        choices=["Spear Grass",
+        "Marmalade Bush",
+        "Golden Bell",
+        "Purple Smokebush",
+        "Butterfly Bush"]
+        )
     date = DateField("Date", validators=[DataRequired()])
     sender = StringField("Sender", validators=[DataRequired(), Length(min=3, max=25)])
     diamonds = IntegerField("Diamonds",validators=[NumberRange(0,20)])
     ribbons = IntegerField("Ribbons",validators=[NumberRange(0,100)])
-
 
