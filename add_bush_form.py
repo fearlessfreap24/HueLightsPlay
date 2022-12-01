e49baca7717bf38682823bb9597cd75a72541037
Dylan Perez <24607891+fearlessfreap24@users.noreply.github.com>
2022-11-30 22:12:55 -0600

    made diamonds a required input

--- add_bush_form.py
+++ add_bush_form.py
@@ -13,6 +13,6 @@ class Add_Bush(FlaskForm):
         )
     date = DateField("Date", validators=[DataRequired()])
     sender = StringField("Sender", validators=[DataRequired(), Length(min=3, max=25)])
-    diamonds = IntegerField("Diamonds",validators=[NumberRange(0,20)])
+    diamonds = IntegerField("Diamonds",validators=[NumberRange(0,20, DataRequired()])
     ribbons = IntegerField("Ribbons",validators=[NumberRange(0,100)])
 
